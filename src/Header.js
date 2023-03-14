e3e02c23eea223e39720b6fe2054be7a78b64ead
s.elkharraz <saif.elkharraz@seven-app.net>
2023-03-14 19:14:00 +0100

    fixes

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,21 @@
-import React, { useContext } from 'react'
-import { RpsContext } from './Contexts/RpsContext'
-import "./styles/header.css"
+import React, { useContext } from "react";
+import { RpsContext } from "./Contexts/RpsContext";
+import "./styles/header.css";
 
 function Header() {
-  const {score} = useContext(RpsContext)
+  const { score } = useContext(RpsContext);
 
   return (
-    <div className='head'>
-        <div className='header'>
-            <div className='game-logo'>
-                <img src="../images/logo.svg" alt="logo" />
-            </div>
-            <div className='score-board'>
-                <div className='score-txt upper'>
-                    score
-                </div>
-                <div className='the-score'>
-                    {score}
-                </div>
-            </div>
-        </div>
+    <div className="header">
+      <div className="game-logo">
+        <img src="../images/logo.svg" alt="logo" />
+      </div>
+      <div className="score-board">
+        <div className="score-txt upper">score</div>
+        <div className="the-score">{score}</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
